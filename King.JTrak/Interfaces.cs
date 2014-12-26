d4af4d7b281085cb24f97123ff5ba0515db407d5
Jef King <jef@agilebusinesscloud.com>
2014-12-26 09:27:25 -0800

    adding framework

---
+++ King.JTrak/Interfaces.cs
@@ -0,0 +1,20 @@
+﻿namespace King.JTrak
+{
+    using System.Threading.Tasks;
+
+    #region ISynchronizer
+    /// <summary>
+    /// Aszure Table Synchronizer Interface
+    /// </summary>
+    public interface ISynchronizer
+    {
+        #region Methods
+        /// <summary>
+        /// Run Synchronization
+        /// </summary>
+        /// <returns>Task</returns>
+        Task Run();
+        #endregion
+    }
+    #endregion
+}
---
+++ King.JTrak/Models/ConfigValues.cs
@@ -0,0 +1,12 @@
+﻿namespace King.JTrak.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ConfigValues : IConfigValues
+    {
+    }
+}
