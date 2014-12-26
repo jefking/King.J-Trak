4b5ea9c4fe83e99e0dcf9302be5e73ac06e0d80a
Jef King <jef@agilebusinesscloud.com>
2014-12-26 09:41:41 -0800

    main algo

--- King.JTrak/Synchronizer.cs
+++ King.JTrak/Synchronizer.cs
@@ -2,8 +2,10 @@
 {
     using King.Azure.Data;
     using King.JTrak.Models;
+    using Microsoft.WindowsAzure.Storage.Table;
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -38,7 +40,24 @@
         /// <returns>Task</returns>
         public async Task Run()
         {
-            await new TaskFactory().StartNew(() => { });
+            Trace.TraceInformation("Initializing: {0}.", this.to.Name);
+            await this.to.CreateIfNotExists();
+
+            Trace.TraceInformation("Loading data from {0}.", this.from.Name);
+            var entities = await this.from.Query(new TableQuery());
+            if (null != entities && entities.Any())
+            {
+                Trace.TraceInformation("Storing data to {0}.", this.to.Name);
+                foreach (var e in entities)
+                {
+                    var name = string.Format("", e[TableStorage.PartitionKey], e[TableStorage.RowKey]);
+                    await this.to.Save(name, e);
+                }
+            }
+            else
+            {
+                Trace.TraceInformation("No entities in {0}.", this.from.Name);
+            }
         }
         #endregion
     }
