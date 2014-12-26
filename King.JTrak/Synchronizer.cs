b34abdccc9a66eb2edd536f6f0b2c263e4932559
Jef King <jef@agilebusinesscloud.com>
2014-12-26 09:39:10 -0800

    framing

--- King.JTrak/Synchronizer.cs
+++ King.JTrak/Synchronizer.cs
@@ -1,16 +1,45 @@
 ﻿namespace King.JTrak
 {
+    using King.Azure.Data;
+    using King.JTrak.Models;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Aszure Table Synchronizer
+    /// </summary>
     public class Synchronizer : ISynchronizer
     {
+        #region Members
+        /// <summary>
+        /// From Azure Table Storage
+        /// </summary>
+        protected readonly ITableStorage from = null;
+
+        /// <summary>
+        /// To Azure Blob Storage
+        /// </summary>
+        protected readonly IContainer to = null;
+        #endregion
+
+        #region Constructors
+        public Synchronizer(ConfigValues config)
+        {
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Run Synchronization
+        /// </summary>
+        /// <returns>Task</returns>
         public async Task Run()
         {
             await new TaskFactory().StartNew(() => { });
         }
+        #endregion
     }
 }
