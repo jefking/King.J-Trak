131c0d67e4adf9e68e85c1fdc298b647b8f3880c
Jef King <jef@agilebusinesscloud.com>
2014-12-26 09:32:17 -0800

    Testing

--- King.JTrak.Unit.Test/Properties/AssemblyInfo.cs
+++ King.JTrak.Unit.Test/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("King.JTrak.Integration.Test")]
+[assembly: AssemblyTitle("King.JTrak.Unit.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("King.JTrak.Integration.Test")]
+[assembly: AssemblyProduct("King.JTrak.Unit.Test")]
 [assembly: AssemblyCopyright("Copyright © Jef King 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("5ff8d2c7-3720-40c6-83e9-13008db8e5db")]
+[assembly: Guid("23001afd-8868-4620-a61d-2edcd8133d3f")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
